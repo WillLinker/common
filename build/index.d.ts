0f17ecce9cdfc938a4cd35e44887962cbb124bff
Will Linker <willlinker@MacMini2018.local>
2021-12-29 13:19:04 -0500

    updates

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,13 +1,9 @@
-/*
- * Added a commit to the index.ts file :-)
- */
 export * from "./errors/BadRequestError";
 export * from "./errors/CustomError";
 export * from "./errors/NotAuthorizedError";
 export * from "./errors/database-connection-error";
 export * from "./errors/not-found-error";
-export * from "./errors/request-validator-error"
-
+export * from "./errors/request-validator-error";
 export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
